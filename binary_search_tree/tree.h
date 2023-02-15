688976822f7bf72ce7b08865e287cdc1a60783fe
Notgard <raoul.behaghie@gmail.com>
2023-02-15 16:20:00 +0100

    modified header

--- binary_search_tree/tree.h
+++ binary_search_tree/tree.h
@@ -6,8 +6,8 @@
 
 #define MAX_SIZE 256+1
 
-#ifndef __tree_H__
-#define __tree_H__
+#ifndef __TREE_H__
+#define __TREE_H__
 
 //struct node_t;
 typedef struct tree_t {
@@ -21,4 +21,4 @@ void bst_print(BST, int);
 struct node_t * bst_search(BST, char[MAX_SIZE]);
 int bst_count_nodes(BST);
 
-#endif //__tree_H__
+#endif //__TREE_H__
